50afc451981483044ab043e630b7e17cc3cf7a3c
urzp <urzp002@mail.ru>
2017-03-27 15:52:43 +0300

    add count near mines and render board

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,4 +1,9 @@
+
 var Sapper = Sapper || {};
+
+Sapper.constants ={};
+// [y,x]
+Sapper.constants.neighbors = [ [-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1] ];
 
 Sapper.Board = {};
 Sapper.constructors ={};
@@ -7,6 +12,7 @@ Sapper.Board.Cells = [];
 Sapper.Board.init_board = function(){
     Sapper.Board.create_cells()
     Sapper.Board.set_numbers()
+    Sapper.Board.render()
 };
 Sapper.Board.create_cells = function(){
 var i,x,y,count_mines=0, mine;
@@ -26,28 +32,55 @@ var i,x,y,count_mines=0, mine;
 Sapper.Board.set_numbers = function(){
     Sapper.Board.Cells.forEach(function(row,i){
         row.forEach(function(cell,x){
-           cell.content = Sapper.Board.count_mines(); 
+            if (cell.content != "mine"){
+                cell.content = Sapper.Board.count_near_mines(cell); 
+            }
+            
         }) 
     });
 
 }
-
-Sapper.Board.count_mines = function(cell){
-
-    
+Sapper.Board.count_near_mines = function(cell){
+    var x,y,mine_count=0;
+    Sapper.constants.neighbors.forEach(function(der,i){
+        y = der[0]+cell.position[0];
+        x = der[1]+cell.position[1];
+        if ((y >= 0)&&(x >= 0)&&(x < 10)&&(y < 10)){
+            //alert( (Sapper.Board.Cells[y][x]).content == "mine" )
+            if ((Sapper.Board.Cells[y][x]).content == "mine"){
+                mine_count++;
+            }
+        }
+    })
+     return mine_count;
+}
+Sapper.Board.render = function(){
+    //$("#cell00").append("<p>1</p>");
+    //alert($("#cell01"));
+    Sapper.Board.Cells.forEach(function(row,i){
+        row.forEach(function(cell,x){
+            id_cell = '#cell'+(cell.position[0]).toString(16)+(cell.position[1]).toString(16);
+            if (cell.content != "mine"){
+                $(id_cell).append("<p>"+(cell.content).toString(16)+"</p>");
+            }else{
+               $(id_cell).append('<img src="img/mine.png" alt="">');
+            }
+            
+        }) 
+    }); 
 }
 
 Sapper.constructors.Cell = function(x,y,content){
-  this.position = [x,y];
+  this.position = [y,x];
   this.open = false;
   this.content = content;
 };
 
 
 
-
-
-Sapper.Board.init_board();
+$('document').ready( function(){
+    Sapper.Board.init_board();
+})
 
 /*
 Sapper.define = function(namespace){
